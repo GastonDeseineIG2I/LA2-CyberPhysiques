3f1de72d041fbc08b520aca2b750270fa2e7400b
Gaston Deseine <gaston.deseine@ig2i.centralelille.fr>
2021-06-09 12:20:45 +0200

    main.py

--- main.py
+++ main.py
@@ -11,22 +11,20 @@ from marvelmind import MarvelmindHedge
 HEDGEID = 54
 
 def main():
-    ser2 = serial.Serial2('/dev/ttyS0', 115200, timeout=1)
+    
+    ser2 = serial.Serial('/dev/ttyS0', 115200, timeout=1) # Arduino <-> RPI
     ser2.flush()
-
-    ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
-    ser.flush()
 
     hedge = setHedge()
     while 1:
         try:
-            if ser.in_waiting > 0:
+            if ser2.in_waiting > 0:
                 # Récuperation de l'angle
-                angle  = ser.readline().decode('utf-8').rstrip()
+                angle  = ser2.readline().decode('utf-8').rstrip()
 
                 # Récuperation de la position
-                hedge.dataEvent.wait(1)
-                hedge.dataEvent.clear()
+                #hedge.dataEvent.wait(1)
+                #hedge.dataEvent.clear()
 
                 if (hedge.positionUpdated):
                     position =  getPosition(hedge)
@@ -36,9 +34,8 @@ def main():
                     print('X:{} Y:{} Z:{} θ:{}'.format(result[0], result[1], result[2], result[3]))
 
                
-                    ser.write(b"2/-2\n")
-                    line = ser.readline().decode('utf-8').rstrip()
-                    time.sleep(1)
+                    ser2.write(b"2/-2\n")
+                    
                 
 
         except KeyboardInterrupt:
